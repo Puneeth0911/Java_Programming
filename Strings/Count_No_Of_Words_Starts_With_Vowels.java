acb4042f0b68a02bc8b760871fa81dd05ed8fbd5
Puneeth0911 <125130983+Puneeth0911@users.noreply.github.com>
2026-09-21 21:51:47 +0530

    Update Count_No_Of_Words_Starts_With_Vowels.java

--- Strings/Count_No_Of_Words_Starts_With_Vowels.java
+++ Strings/Count_No_Of_Words_Starts_With_Vowels.java
@@ -2,6 +2,15 @@ package Strings;
 
 public class Count_No_Of_Words_Starts_With_Vowels {
 	public static void main(String[] args) {
-		
+		String s="apple banana orange mango";
+		String[] words=s.split(" ");
+		int count=0;
+		for(String word:words) {
+			char ch=word.charAt(0);
+			if(ch=='a' || ch=='e' ||ch=='i' || ch=='o' || ch=='u' || ch=='A' || ch=='E' ||ch=='I' || ch=='O' || ch=='U') {
+				count++;
+			}  
+		}
+		System.out.println(count);
 	}
 }
